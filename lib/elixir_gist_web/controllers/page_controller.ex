de09afbd5dda1dd6ef12928a1f3dd19f7c90c88f
Haitham314s <haitham314s@gmail.com>
2024-03-04 23:34:41 +0400

    Gist form creation with live view

--- lib/elixir_gist_web/controllers/page_controller.ex
+++ lib/elixir_gist_web/controllers/page_controller.ex
@@ -2,10 +2,6 @@ defmodule ElixirGistWeb.PageController do
   use ElixirGistWeb, :controller
 
   def home(conn, _params) do
-    # redirect(conn, to: "/create")
-
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home)
+    redirect(conn, to: "/create")
   end
 end
--- lib/elixir_gist_web/live/create_gist_live.ex
+++ lib/elixir_gist_web/live/create_gist_live.ex
@@ -1,9 +1,20 @@
 defmodule ElixirGistWeb.CreateGistLive do
-  alias ElixirGist.Accounts
   use ElixirGistWeb, :live_view
 
+  alias ElixirGist.Accounts
+  alias ElixirGist.{Gists, Gists.Gist}
+
   def mount(_params, %{"user_token" => user_token}, socket) do
-    socket = assign(socket, current_user: Accounts.get_user_by_session_token(user_token))
+    current_user = Accounts.get_user_by_session_token(user_token)
+    gist_form = to_form(Gists.change_gist(%Gist{}))
+
+    socket =
+      assign(
+        socket,
+        current_user: current_user,
+        form: gist_form
+      )
+
     {:ok, socket}
   end
 end
