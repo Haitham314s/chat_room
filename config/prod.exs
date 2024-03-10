4cc7d85641c213766bdfd4f12e108d2cd06b1e95
haitham314s <haitham314s@gmail.com>
2024-03-10 12:47:55 +0400

    Setup prod config for gigalixir

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :elixir_gist, ElixirGistWeb.Endpoint,
-url: [host: "https://elixir-gist.gigalixirapp.com/", scheme: "https", port: 443],
+  url: [host: "https://elixir-gist.gigalixirapp.com/", scheme: "https", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   check_origin: ["https://elixir-gist.gigalixirapp.com/"],
   cache_static_manifest: "priv/static/cache_manifest.json"
